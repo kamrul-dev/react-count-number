b1fbb66471ee264683e58ac26dd3e7e2c4be39b2
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-17 16:05:28 +0600

    Counting mini project in react done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1>Counting numbers</h1>
+      <Counting></Counting>
     </div>
   );
 }
+
+function Counting() {
+  const [counts, setCount] = useState(0);
+  const increaseNumber = () => { setCount(counts + 1) }
+  const decreaseNumber = () => {
+    if (counts > 0) {
+      setCount(counts - 1)
+    }
+  }
+  return (
+    <div>
+      <h2>Count: {counts}</h2>
+      <button onClick={increaseNumber}>Inrease</button>
+      <button onClick={decreaseNumber}>Decrease</button>
+    </div>
+  )
+}
+
+
 
 export default App;
